9a0988304a9154fda4f8d7294e3d1e1a04b8333b
HishamKhalil1990 <hisham.khalil1990@gmail.com>
2021-04-12 10:55:43 +0300

    table added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV;
 const express = require("express");
 const superagent = require("superagent");
 const cors = require("cors");
@@ -11,19 +12,16 @@ app.use(cors());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.set('view engine', 'ejs');
-// const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
-// const client = new pg.Client(options);
-// client.on('error', error => { throw error; })
-// client.connect().then(() => {
-//     app.listen(PORT, () => {
-//         console.log('we are listening to port 3000')
-//     })
-// }).catch(error => {
-//     console.log("client connction faild");
-// })
-app.listen(PORT, () => {
-    console.log('we are listening to port 3000')
-});
+const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
+const client = new pg.Client(options);
+client.on('error', error => { throw error; })
+client.connect().then(() => {
+    app.listen(PORT, () => {
+        console.log('we are listening to port 3000')
+    })
+}).catch(error => {
+    console.log("client connction faild");
+})
 app.get('/',homePage);
 app.get('/search',searchPage);
 app.get('/location',getLocation);
